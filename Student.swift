5fbd90e216c5c196adae319abca67b6d53f7cc36
DML <darrenm.leith@lethalapps.com>
2016-02-16 13:28:30 +0000

    MVC file structure created

---
+++ Student.swift
@@ -0,0 +1,13 @@
+//
+//  Student.swift
+//  OnTheMap
+//
+//  Created by Darren Leith on 16/02/2016.
+//  Copyright © 2016 Darren Leith. All rights reserved.
+//
+
+import UIKit
+
+struct Student {
+	
+}

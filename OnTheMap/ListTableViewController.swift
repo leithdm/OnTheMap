2f14fcac9a190ccfcf7980c989746d73a6e9bbef
DML <darrenm.leith@lethalapps.com>
2016-02-17 14:38:40 +0000

    improved type safety / clicking on row opens URL / included alert controller for URL's that have errors, which seems to be quite a lot frankly....

--- OnTheMap/ListTableViewController.swift
+++ OnTheMap/ListTableViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class ListTableViewController: UITableViewController {
 	
-	
+	var students: [Student]?
+	var sharedSession: ParseClient?
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		sharedSession = ParseClient.sharedInstance
+		students = sharedSession?.students
 	}
 	
 	// MARK: - Table view data source
@@ -24,19 +27,62 @@ class ListTableViewController: UITableViewController {
 	}
 	
 	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return ParseClient.sharedInstance.students!.count
+		if let count = students?.count {
+			return count
+		} else {
+			return 0
+		}
 	}
 	
-	
+	//cell for row at index path
 	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
-		
-		let student = ParseClient.sharedInstance.students![indexPath.row]
-		cell.textLabel!.text = "\(student.firstName!) \(student.lastName!)"
-		cell.detailTextLabel!.text = "\(student.mapString!) | \(student.mediaURL!)"
-		cell.imageView?.image = UIImage(named: "user")
-		
+		if let students = students {
+			let student = students[indexPath.row]
+			if let firstName = student.firstName, lastName = student.lastName, mapString = student.mapString, mediaURL = student.mediaURL {
+				cell.textLabel!.text = "\(firstName) \(lastName)"
+				cell.detailTextLabel!.text = "\(mapString) | \(mediaURL)"
+				cell.imageView?.image = UIImage(named: "user")
+			}
+		}
 		return cell
+	}
+	
+	//did select row
+	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		if let students = students {
+			if let URL = students[indexPath.row].mediaURL {
+				let app = UIApplication.sharedApplication()
+				if let url = NSURL(string: URL) {
+					if app.canOpenURL(url) {
+						app.openURL(url)
+					} else {
+						showAlertViewController("Whoops!", message: "Looks like this student put in an incorrect URL")
+					}
+				}
+			}
+		}
+	}
+	
+	//show an AlertViewController
+	func showAlertViewController(title: String? , message: String?) {
+		
+		performUIUpdatesOnMain {
+			//self.activityIndicator.stopAnimating()
+			if title != nil && message != nil {
+				let errorAlert =
+				UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+				errorAlert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+				self.presentViewController(errorAlert, animated: true, completion: nil)
+			}
+		}
+	}
+	
+	//run on main thread
+	func performUIUpdatesOnMain(updates: () -> Void) {
+		dispatch_async(dispatch_get_main_queue()) {
+			updates()
+		}
 	}
 	
 }
